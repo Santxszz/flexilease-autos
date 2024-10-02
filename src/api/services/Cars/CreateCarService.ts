71a42eb0f4393df62bfdf36b9663cdc8af667195
Erek Couto <erekcoutosantos22@gmail.com>
2024-10-02 13:58:53 -0300

    feat: added createCarService

---
+++ src/api/services/Cars/CreateCarService.ts
@@ -0,0 +1,60 @@
+import bcrypt from "bcrypt";
+import dayjs from "dayjs";
+
+import { AppDataSource } from "@database/index";
+import AppError from "@api/middlewares/AppError";
+import nameValidation from "@api/utils/nameValidation";
+import Car from "@database/entities/Car";
+
+import type InterfaceRequestCarCreate from "@api/interfaces/InterfaceRequestCarCreate";
+
+export default class CreateCarService {
+	public async execute({
+		model,
+		color,
+		year,
+		valuePerDay,
+		acessories,
+		numberOfPassengers,
+	}: InterfaceRequestCarCreate) {
+		const userRepository = AppDataSource.getRepository(Car);
+
+		const acessoriesArray = [];
+		for (let i = 0; i < acessories.length; i++) {
+			acessoriesArray.push(acessories[i].name);
+		}
+        
+		if (year < 1950 || year > 2023) {
+			throw new AppError(
+				"The year of vehicle is invalid, must be between 1930-2023!",
+			);
+		}
+
+		if (acessories.length < 1) {
+			throw new AppError("Requires at least one accessory!");
+		}
+
+		const createCar = userRepository.create({
+			model,
+			color,
+			year,
+			valuePerDay,
+			acessories: acessoriesArray,
+			numberOfPassengers,
+		});
+
+		await userRepository.save(createCar);
+
+		const resultObjectCar = {
+			id: createCar.id,
+			model: createCar.model,
+			color: createCar.color,
+			year: createCar.year,
+			valuePerDay: createCar.valuePerDay,
+			acessories: acessories,
+			numberOfPassengers: numberOfPassengers,
+		};
+
+		return resultObjectCar;
+	}
+}
