bf3877de3c80c579fcf0d603123cfa7961accbc9
Erek Couto <erekcoutosantos22@gmail.com>
2024-10-02 02:29:57 -0300

    feat: added middleware of auth verify and ended any controllers of endpoints

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import jwt from "jsonwebtoken";
 import type { Request, Response } from "express";
 import { instanceToInstance } from "class-transformer";
 
@@ -7,6 +8,9 @@ import CreateUserService from "@api/services/Users/CreateUserService";
 import calcQualified from "@api/utils/verifyQualifyCategory";
 import AppError from "@api/middlewares/AppError";
 import UserAuthService from "@api/services/Users/UserAuthService";
+import UpdateUserService from "@api/services/Users/UpdateUserService";
+import type JwtPayload from "@api/interfaces/InterfaceJwtPayload";
+import DeleteUserService from "@api/services/Users/DeleteUserService";
 
 export default class UserController {
 	public async create(req: Request, res: Response): Promise<Response> {
@@ -50,8 +54,63 @@ export default class UserController {
 		const { email, password } = req.body;
 		const authService = new UserAuthService();
 
-        const acessToken = await authService.execute({email, password})
-        return res.status(200).json({acessToken: acessToken})
+		const acessToken = await authService.execute({ email, password });
+		return res.status(200).json({ acessToken: acessToken });
+	}
 
+	public async update(req: Request, res: Response): Promise<Response> {
+		const { name, cpf, birth, cep, email, password } = req.body;
+		const id = Number(req.params.id);
+		const userToken = req.headers.authorization;
+
+		jwt.verify(
+			userToken as string,
+			process.env.JWT_SECRET as string,
+			(err, userInfo) => {
+				if (err) {
+					throw new AppError("Token is Invalid", 400);
+				}
+				const tokenPayload = userInfo as JwtPayload;
+				if (tokenPayload.userId !== id) {
+					throw new AppError("Not Authorized", 401);
+				}
+			},
+		);
+
+		const userService = new UpdateUserService();
+		const updatedUser = await userService.execute({
+			name,
+			cpf,
+			birth,
+			cep,
+			email,
+			password,
+			id,
+		});
+
+		return res.status(200).json(updatedUser);
+	}
+
+	public async delete(req: Request, res: Response): Promise<Response> {
+		const userToken = req.headers.authorization;
+		const id = Number(req.params.id);
+
+		jwt.verify(
+			userToken as string,
+			process.env.JWT_SECRET as string,
+			(err, userInfo) => {
+				if (err) {
+					throw new AppError("Token is Invalid", 400);
+				}
+				const tokenPayload = userInfo as JwtPayload;
+				if (tokenPayload.userId !== id) {
+					throw new AppError("Not Authorized", 401);
+				}
+			},
+		);
+
+		const userService = new DeleteUserService();
+		await userService.execute({ id });
+		return res.status(204).json();
 	}
 }
