e2e910f9723b5c1a9a8f1da0a754014e3d3fb925
Erek Couto <erekcoutosantos22@gmail.com>
2024-10-02 13:59:53 -0300

    feat: created Middleware checkUserToken and Interface for request createCar

---
+++ src/api/middlewares/CheckUserAuth.ts
@@ -0,0 +1,29 @@
+import type { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+import AppError from "./AppError";
+import type JwtPayload from "@api/interfaces/InterfaceJwtPayload";
+
+export default async function checkUserAuth(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	const userToken = req.headers.authorization;
+	const bearerToken = userToken?.split(" ")[1];
+	const userId = Number(req.params.id);
+
+	jwt.verify(
+		bearerToken as string,
+		process.env.JWT_SECRET as string,
+		(err, userInfo) => {
+			if (err) {
+				throw new AppError("Token is Invalid", 400);
+			}
+			const tokenPayload = userInfo as JwtPayload;
+			if (tokenPayload.userId !== userId) {
+				throw new AppError("Not Authorized", 401);
+			}
+		},
+	);
+    next()
+}
