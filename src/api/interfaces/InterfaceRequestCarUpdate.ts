08acae928b746f41f02648755ad35d68a9919185
Erek Couto <erekcoutosantos22@gmail.com>
2024-10-03 12:20:15 -0300

    feat: interface for car update request

---
+++ src/api/interfaces/InterfaceRequestCarUpdate.ts
@@ -0,0 +1,9 @@
+export default interface InterfaceRequestCarUpdate {
+	model?: string;
+	color?: string;
+	year?: number;
+	valuePerDay?: number;
+	acessories?: string[];
+	numberOfPassengers?: number;
+	id?: number;
+}
