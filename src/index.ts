aa51e7029f9b020d99371a2d1e6e267d5debc8e4
Erek Couto <erekcoutosantos22@gmail.com>
2024-10-04 02:00:24 -0300

    feat: enabled cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import "reflect-metadata";
 import "express-async-errors";
-import express, { type Response, type Request, type NextFunction } from "express";
+import cors from "cors";
+import express, {
+	type Response,
+	type Request,
+	type NextFunction,
+} from "express";
 import "dotenv/config";
 import { errors } from "celebrate";
 import swaggerUI from "swagger-ui-express";
@@ -12,9 +17,9 @@ import AppError from "@api/middlewares/AppError";
 import userRoutes from "./routes/Users.routes";
 import carRoutes from "@routes/Cars.routes";
 
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,25 +30,22 @@ app.use("/v1/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
 app.use(errors());
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            code: error.statusCode,
-            status: error.statusMessage,
-            message: error.message,
-        });
-    }
+app.use(
+	(error: Error, request: Request, response: Response, next: NextFunction) => {
+		if (error instanceof AppError) {
+			return response.status(error.statusCode).json({
+				code: error.statusCode,
+				status: error.statusMessage,
+				message: error.message,
+			});
+		}
 
-    return response.status(500).json({
-        code: 500,
-        status: "Internal Server Error",
-        message: "Ocorreu um erro inesperado",
-    });
-    
-});
+		return response.status(500).json({
+			code: 500,
+			status: "Internal Server Error",
+			message: "Ocorreu um erro inesperado",
+		});
+	},
+);
 
-const PORT = process.env.PORT_SERVER || 3000;
-app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`[🤖] API: COMPASSCINE - ONLINE - PORTA: ${PORT}`);
-});
+module.exports = app;
